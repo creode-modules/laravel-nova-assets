1db62d4767a57a946811fca3892362c742498f13
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-11-03 12:31:16 +0000

    Send user a notification when bulk uploads have succeeded.

---
+++ src/Notifications/BulkUploadSuccessNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Creode\LaravelNovaAssets\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class BulkUploadSuccessNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(protected string $successMessage)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line($this->successMessage);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
